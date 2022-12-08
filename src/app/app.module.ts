d8ba56edaa5b2c41259bc181ee767203177f6846
AlexanderCasanova <alexandercasanova2000@outlook.com>
2022-12-08 11:28:17 -0500

    commit1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+
+
+const appRoutes: Routes=[
+  {path:'home',component: HomeComponent},
+  {path:'categories',component: CategoriesComponent},
+  {path:'login',component: LoginComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
